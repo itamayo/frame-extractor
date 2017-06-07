1306a3089c3d3473857536d884185efa05400463
itamayo <tamaxx@gmail.com>
2017-06-07 09:49:47 +0200

    Update index.js

--- index.js
+++ index.js
@@ -40,27 +40,19 @@ function calculateFrame(frameRate, startTime, time, frameNumber) {
 }
 
 module.exports =  {
-  extractFrame: function(filePath, time, frameNumber, outputPath) {
-    console.log(time);
+  extractFrame: function(filePath, numOfFrames, frameNumber, outputPath) {
     console.log(frameNumber);
     return new Promise(function(resolve, reject) {
       var frame, command, videoTrack, timecodeTrack, startTimecode, fileName;
-
-      startTimecode = '00:00:00';
    
       getMediaInfo(filePath).then(function(info) {
 	console.log(info.file.track[1]);
 	info = info.file;
         videoTrack = info.track.filter(function(track) { console.log(track._type);return track._type === "Video"; })[0];
-	console.log(videoTrack);
-        timecodeTrack = _.select(info.track, function(track) { return track._type === "OtherTime code" })[0];
-
-        if(timecodeTrack !== null && timecodeTrack !== undefined)
-          startTimecode = timecodeTrack.time_code_of_first_frame;
 	console.log(videoTrack.frameRate);
-        frame = calculateFrame(videoTrack.frameRate, startTimecode, time, parseInt(frameNumber));
+        frame =  parseInt(frameNumber);
 
-        command  = 'time ffmpeg -i ' + filePath +' -s 192x168 -qscale 28 -vf "select=gte(n\\, ' + frame + ')" -vframes 1 -threads 10 /tmp/' + outputPath + ' -y';
+        command  = 'time ffmpeg -loglevel panic -i ' + filePath +' -s 192x168 -qscale 28 -vf "select=gte(n\\, ' + frame + ')" -vframes '+numOfFrames+' -threads 10 /tmp/' + outputPath + ' -y';
         child = exec(command, function (error, stdout, stderr) {
           if (error !== null) {
             console.log('exec error: ' + error);
