a82d532efeb814b69fa82e0fdde7e2a2a9dd80e5
itamayo <tamaxx@gmail.com>
2021-09-23 17:54:51 +0200

    Update index.js

--- test/index.js
+++ test/index.js
@@ -1,2 +1,2 @@
 var frameExtractor = require('../index');
-frameExtractor.extractFrame('/var/www/html/24fps.mp4', 5 , 1, 'frame%04d.jpg');
+frameExtractor.extractFrame('http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/BigBuckBunny.mp4', 5 , 1, 'frame%04d.jpg');
